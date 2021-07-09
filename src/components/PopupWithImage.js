80a36fe79b4a248b5e6c4ba7a1cb28437bd2ea06
iogonchar <i.o.gonchar@yandex.ru>
2021-07-10 00:24:54 +0300

    fix: popup with image elements moved to constructor

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -1,14 +1,19 @@
 import Popup from './Popup.js';
 
 export default class PopupWithImage extends Popup {
+  constructor(popupSelector) {
+    super(popupSelector);
+
+    this._popupImg = this._popup.querySelector('.popup__img');
+    this._popupText = this._popup.querySelector('.popup__text');
+  }
+
   open(title, imgLink) {
-    const popupImg = this._popup.querySelector('.popup__img');
-    const popupText = this._popup.querySelector('.popup__text');
 
     // указание на изображение и подпись для попапа
-    popupImg.src = imgLink;
-    popupImg.alt = title;
-    popupText.textContent = title;
+    this._popupImg.src = imgLink;
+    this._popupImg.alt = title;
+    this._popupText.textContent = title;
 
     // Открытие попапа
     super.open();
